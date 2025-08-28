1adffc0db7d09e054029524e450d1d2f994c6a80
edwindezak <edwindezak@gmail.com>
2025-08-28 13:12:03 +0000

    sorting the univeristy image

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -21,7 +21,7 @@ export const projects: Project[] = [
       "This portal digitizes university results to eliminate manual processing and printing. Students can conveniently access their results from home, ensuring a streamlined and efficient academic experience.",
     category: "University",
     Icon: School,
-    image: "https://www.pexels.com/photo/silhouette-of-people-raising-their-graduation-hats-8093032/",
+    image: "https://images.pexels.com/photos/159213/hall-congress-architecture-building-159213.jpeg",
     aiHint: "university campus",
     liveUrl: "https://university-frontend-t3q7.onrender.com/",
     repoUrl: "https://github.com/zakoshy/university-frontend",
